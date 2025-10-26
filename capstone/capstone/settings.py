5a7aa58cd6fb6b45e19e93ca8c24414181f97526
Wuhib Mezemir <86829490+wuhibm@users.noreply.github.com>
2025-10-26 13:38:38 -0400

    Restructured directory

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
-
+#SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("DEBUG", default=False))
 
